3c7dadd70cc5a51d9674d986fbeb91d08b4a4d75
edp-robot <119433300+edp-robot@users.noreply.github.com>
2026-01-08 14:53:35 +0200

    Initial commit

---
+++ Makefile
@@ -0,0 +1,5 @@
+.PHONY: test
+
+test: ## Run JUnit tests
+	# Execute all tests in project
+	mvn test
---
+++ src/test/java/com/epam/edp/demo/HelloTest.java
@@ -0,0 +1,11 @@
+package com.epam.edp.demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HelloTest {
+    @Test
+    public void testHello() {
+        assertEquals(2, 1 + 1);
+    }
+}
